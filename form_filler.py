3315b726632467519e47c0390e6e5ba9d0e984da
coder teddi <greg.choww@gmail.com>
2024-03-27 12:10:24 -0700

    refactored code, working search and pytests

--- form_filler.py
+++ form_filler.py
@@ -1,47 +1,20 @@
 #!/usr/bin/env python3
 
-import tkinter as tk
-
 import os
 import pandas as pd
 import yaml
+import tkinter as tk
+import requests
 
-import time
+
+from datetime import datetime
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import WebDriverException
 
 
-def on_keyrelease(event):
-
-    # get text from entry
-    value = event.widget.get()
-    value = value.strip().lower()
-
-    # get data from test_list
-    if value == '':
-        data = list
-    else:
-        data = []
-        for item in list:
-            if value in item.lower():
-                data.append(item)
-
-    # update data in listbox
-    listbox_update(data)
-
-
-def listbox_update(data, listbox):
-    # delete previous data
-    listbox.delete(0, 'end')
-
-    # sorting data
-    data = sorted(data, key=str.lower)
-
-    # put new data
-    for item in data:
-        listbox.insert('end', item)
-
-
+# Display selected item in console
 def on_select(event):
     # display element selected on list
     print('(event) previous:', event.widget.get('active'))
@@ -49,12 +22,7 @@ def on_select(event):
     print('---')
 
 
-# def find_row_number(text, search_string):
-#     lines = text.split('\n')
-#     for i, line in enumerate(lines, 0):
-#         if search_string in line:
-#             return i
-#     return None
+# Find row of the search string
 def find_row(data, search_string):
     for i in range(len(data)):
         if search_string in data.iloc[i].values:
@@ -62,11 +30,85 @@ def find_row(data, search_string):
     return None
 
 
-# def find_col(data, search_string):
-#     for column in data.columns:
-#         if data[column].eq(search_string).any():
-#             return column
-#     return None
+def load_config(yml_file_path):
+    try:
+        with open(yml_file_path, 'r') as yml_file:
+            return yaml.safe_load(yml_file)
+    except Exception:
+        print(f'Unable to open: {yml_file_path}')
+        return None
+
+
+def get_config_value(config, key):
+    try:
+        return config[key]
+    except KeyError:
+        print(f'No {key} set in config.yml')
+        return None
+
+
+def read_excel_file(form_file_path):
+    try:
+        return pd.read_excel(form_file_path)
+    except Exception:
+        print(f'Unable to read: {form_file_path}')
+        return None
+
+
+def get_list(data, list_term):
+    try:
+        return data[list_term]
+    except KeyError:
+        print(f'Unable to find: {list_term} in form')
+        return None
+
+
+# Main form filling function
+def fill(event, data, listbox, config):
+    # Load values from config.yml
+    website = get_config_value(config, 'website')
+    terms = get_config_value(config, 'terms')
+    last_form = get_config_value(config, 'last_form')
+    user = get_config_value(config, 'user')
+    # Get the selected item
+    selected_item = listbox.get(listbox.curselection())
+    # Find row of selected term
+    row = find_row(data, f"{selected_item}")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        # Check website status
+        response = requests.get(website)
+        if response.status_code == 200:
+            driver.get(website)
+        else:
+            print(website + ' gave a ' + str(response.status_code)
+                            + ' error code')
+            driver.quit()
+        # Go through list of given terms
+        for dictionary in terms:
+            for key, value in dictionary.items():
+                # Given an id and term name, fill in form from excel sheet
+                try:
+                    tmp = driver.find_element('id', f"{key}")
+                    tmp.send_keys(data.loc[row, f"{value}"])
+                except Exception:
+                    print('id: ' + f"{key}" + ' not found in form')
+
+        # Fill in notes
+        if last_form:
+            try:
+                tmp = driver.find_element('id', last_form)
+            except Exception:
+                print('id: ' + last_form + ' not found in form')
+
+            tmp.send_keys(datetime.today().strftime('%Y-%m-%d') + '\n')
+            tmp.send_keys(user + '\n')
+
+    except WebDriverException:
+        print("Browser window was closed. Cleaning up...")
+        driver.quit()
 
 
 def main():
@@ -76,45 +118,61 @@ def main():
     # Specify config.yml file
     yml_file_name = 'config.yml'
 
-    def fill(event):
-        # Get the selected item
-        selected_item = listbox.get(listbox.curselection())
-        # Find row of selected term
-        row = find_row(data, f"{selected_item}")
-        # Open website
-        driver = webdriver.Chrome()
-        driver.get(website)
-        # Go through list of given terms
-        for dictionary in terms:
-            for key, value in dictionary.items():
-                # Given an id and term name, fill in form from excel sheet
-                tmp = driver.find_element('id', f"{key}")
-                tmp.send_keys(data.loc[row, f"{value}"])
+    # Filter list via input
+    def on_keyrelease(event):
 
-        time.sleep(20)
+        # get text from entry
+        value = event.widget.get()
+        value = value.strip().lower()
+
+        # get data from test_list
+        if value == '':
+            data = list
+        else:
+            data = []
+            for item in list:
+                if value in item.lower():
+                    data.append(item)
+
+        # update data in listbox
+        listbox_update(data, listbox)
+
+    # Update displayed list
+    def listbox_update(data, listbox):
+        # delete previous data
+        listbox.delete(0, 'end')
+
+        # sorting data
+        data = sorted(data, key=str.lower)
+
+        # put new data
+        for item in data:
+            listbox.insert('end', item)
 
     # Create the full file path for the .yml file
     yml_file_path = os.path.join(current_dir, yml_file_name)
 
-    # Read the .yml file
-    with open(yml_file_path, 'r') as yml_file:
-        config = yaml.safe_load(yml_file)
+    # Load the config
+    config = load_config(yml_file_path)
+    if config is None:
+        # Handle error (e.g., exit the program)
+        pass
 
-    # Get the .xlsx file name from the .yml file
-    form_name = config['form_name']
-
-    # Get the website URL
-    website = config['website']
-    terms = config['terms']
-    list_term = config['list_term']
+    # Get the values from the config
+    form_name = get_config_value(config, 'form_name')
+    list_term = get_config_value(config, 'list_term')
 
     # Create the full file path for the .xlsx file
     form_file_path = os.path.join(current_dir, form_name)
 
     # Read the .xlsx file
-    data = pd.read_excel(form_file_path)
+    data = read_excel_file(form_file_path)
+    if data is None:
+        # Handle error (e.g., exit the program)
+        pass
+
     # Get list of company names using list_term from .yml
-    list = data[list_term]
+    list = get_list(data, list_term)
 
     # Create the root window
     root = tk.Tk()
@@ -132,13 +190,16 @@ def main():
     listbox = tk.Listbox(root, bg='black', fg='white')
     # Pack the Listbox widget to make it visible
     listbox.pack()
+    # Add title
+    root.title('Form Filler')
     # Bind the ListboxSelect event to the on_select function
     listbox.bind('<<ListboxSelect>>', on_select)
     # Update the Listbox widget with the list of items
     listbox_update(list, listbox)
 
     # Bind the double left mouse button click to fill function
-    listbox.bind('<Double-1>', fill)
+    listbox.bind('<Double-1>', lambda event:
+                 fill(event, data, listbox, config))
 
     # Start the main event loop
     root.mainloop()
